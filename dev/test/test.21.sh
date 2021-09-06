ef3ec305cfa4e5a695868c3c712e31c6ab9a6f8b
edwinjhlee <edwin.jh.lee@gmail.com>
2021-09-06 11:57:27 +0800

    ...

--- dev/test/test.21.sh
+++ dev/test/test.21.sh
@@ -28,6 +28,8 @@ test123(){
 # time test123 "$dir/21.json" user create --username "c:"
 # cat "$dir/21.json"
 time test123 "$dir/21.json" "+"
+time test123 "$dir/21.json" --value ""
+time test123 "$dir/21.json" --value "+"
 
 # time test123 "$dir/3.json" repo --debug --repo abc --
 # time test123 "$dir/3.json" repo --debug --repo abc -
