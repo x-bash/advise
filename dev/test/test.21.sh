713f4fd794c7935bb36b69a76c0004ebd79e164e
edwinjhlee <edwin.jh.lee@gmail.com>
2021-09-06 12:34:23 +0800

    ...

--- dev/test/test.21.sh
+++ dev/test/test.21.sh
@@ -31,6 +31,8 @@ time test123 "$dir/21.json" "+"
 time test123 "$dir/21.json" --value ""
 time test123 "$dir/21.json" --value "+"
 
+time test123 "$dir/21.json" --value +a ""
+
 # time test123 "$dir/3.json" repo --debug --repo abc --
 # time test123 "$dir/3.json" repo --debug --repo abc -
 # time test123 "$dir/3.json" repo --debug --repo abc 1 ""
